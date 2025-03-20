5d659d185632ad59b49f59e1142b7fe09a7bd850
Nicolas MASSART <nicolas.massart@consensys.net>
2025-03-20 15:53:24 +0100

    remove unused

--- app/components/Views/SampleFeature/SampleFeature.styles.ts
+++ app/components/Views/SampleFeature/SampleFeature.styles.ts
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import { fontStyles } from '../../../styles/common';
 import { Colors } from '../../../util/theme/models';
 
 const createStyles = (colors: Colors) =>
